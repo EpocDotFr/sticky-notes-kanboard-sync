229bb717ef7a6b3353b70b650e3117fab2d07591
EpocDotFr <contact@epoc.fr>
2016-11-18 18:04:33 +0100

    Kanboard client

--- kanboard.py
+++ kanboard.py
@@ -1,32 +1,39 @@
 from jsonrpcclient.http_client import HTTPClient
+from jsonrpcclient.request import Request
 from jsonrpcclient import config
 from envparse import env
 
 config.validate = False
 
-def send_request(method_name, *args, **kwargs):
+def send_request(request):
     """Send a JSON-RPC request to Kanboard."""
     jsonrpc_client = HTTPClient(env('KANBOARD_ENDPOINT'))
     jsonrpc_client.session.auth = ('jsonrpc', env('KANBOARD_TOKEN'))
 
-    return jsonrpc_client.request(method_name, *args, **kwargs)
+    return jsonrpc_client.send(request)
 
 
 def create_task(title, description=None, color_id=None):
-    return send_request('createTask',
-                        title=title,
-                        description=description,
-                        color_id=color_id,
-                        project_id=env('KANBOARD_PROJECT_ID'),
-                        column_id=env('KANBOARD_COLUMN_ID', default=None),
-                        swimlane_id=env('KANBOARD_SWIMLANE_ID', default=None)
-                        )
+    return Request('createTask',
+                   title=title,
+                   description=description,
+                   color_id=color_id,
+                   project_id=env('KANBOARD_PROJECT_ID'),
+                   column_id=env('KANBOARD_COLUMN_ID', default=None),
+                   swimlane_id=env('KANBOARD_SWIMLANE_ID', default=None)
+                   )
 
 
-def update_task(id, title, description=None, color_id=None):
-    return send_request('updateTask',
-                        id=id,
-                        title=title,
-                        description=description,
-                        color_id=color_id
-                        )
+def update_task(id, title=None, description=None, color_id=None):
+    return Request('updateTask',
+                   id=id,
+                   title=title,
+                   description=description,
+                   color_id=color_id
+                   )
+
+
+def remove_task(id):
+    return Request('removeTask',
+                   task_id=id
+                   )
